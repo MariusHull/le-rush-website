7e714af2aab75e7e84766c2db7f93990660d2a97
MariusHull <marius.hullin@gmail.com>
2018-11-25 20:34:51 +0100

    Implémentation Page Acceuil
    Page d'acceuil implémentée comme HTLM à partir du template de base Parallax de Materialize.JS. Il reste encore à la transformer en composant

---
+++ front/src/Home.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        
+
+
+        <div id="index-banner" class="parallax-container">
+          <div class="section no-pad-bot">
+            <div class="container">
+              <br></br>
+              <h1 class="header center teal-text text-lighten-2">Le Rush</h1>
+              <div class="row center">
+                <h5 class="header col s12 light">
+                  Un festival intense sur le cours d'un week-end. 
+                  Le but : 24 heures pour un court métrage
+                </h5>
+              </div>
+              <div class="row center">
+                <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">L'édition 2019</a>
+              </div>
+              <br></br>
+      
+            </div>
+          </div>
+          <div class="parallax"><img src="IMGLens.jpg"></img></div>
+        </div>
+      
+      
+        <div class="container">
+          <div class="section">
+      
+
+            <div class="row">
+              <div class="col s12 m4">
+                <div class="icon-block">
+                  <h2 class="center brown-text"><i class="material-icons">location_city</i></h2>
+                  <h5 class="center">Sur le campus de CentraleSupélec</h5>
+      
+                  <p class="light">
+                    Le festival se déroule sur le campus de CentraleSupélec qui comprend des installations de pointe.
+                    Vous y serez nourris et logés durant tout l'événement.
+                  </p>
+                </div>
+              </div>
+      
+              <div class="col s12 m4">
+                <div class="icon-block">
+                  <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+                  <h5 class="center">Un moment de partage</h5>
+      
+                  <p class="light">
+                    Retrouvez vous avec plusieurs équipes audiovisuelles venant de toute la France.
+                    Mesurez vous à eux sans relâche durant la compétition, puis vous pourrez échanger avec eux durant le DéRush, à la fin du festival.
+                  </p>
+                </div>
+              </div>
+      
+              <div class="col s12 m4">
+                <div class="icon-block">
+                  <h2 class="center brown-text"><i class="material-icons">camera</i></h2>
+                  <h5 class="center">Repoussez vos limites</h5>
+      
+                  <p class="light">
+                    Le Rush est l'occasion parfaite de montrer ce que vous savez faire, mais aussi d'expérimenter de nouvelles techniques afin d'arriver en première place.
+                  </p>
+                </div>
+              </div>
+            </div>
+      
+          </div>
+        </div>
+      
+
+
+        <div id="index-banner" class="parallax-container">
+            <div class="section no-pad-bot">
+              <div class="container">
+                <br></br>
+                <h1 class="header center teal-text text-lighten-2">Une date : 1-3 février 2019 </h1>
+                <div class="row center">
+                  <h5 class="header col s12 light">
+                    Cette année, Le Rush se déroule sur le Week-End du vendredi 1er au dimanche 3 février... Save the date! 
+                  </h5>
+                </div>
+                <div class="row center">
+                  <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">S'inscrire</a>
+                </div>
+                <br></br>
+        
+              </div>
+            </div>
+            <div class="parallax"><img src="IMGLens.jpg"></img></div>
+          </div>
+      
+        <div class="container">
+          <div class="section">
+      
+            <div class="row">
+              <div class="col s12 center">
+                <h3><i class="mdi-content-send brown-text"></i></h3>
+                <h4>Contact Us</h4>
+                <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+              </div>
+            </div>
+      
+          </div>
+        </div>
+      
+      
+        <div class="parallax-container valign-wrapper">
+          <div class="section no-pad-bot">
+          </div>
+          <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+
+
+
+
+export default Home;
