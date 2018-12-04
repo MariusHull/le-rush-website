2a5c6c0087cbe366eaf0e48ec2a9aa714fa88e0d
MariusHull <marius.hullin@gmail.com>
2018-12-04 17:14:04 +0100

    Update app.js

--- back/app.js
+++ back/app.js
@@ -15,7 +15,6 @@ var equipeRouter = require('./routes/equipes');
 var app = express();
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Origin', 'http://rush.cs-campus.fr:80');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
